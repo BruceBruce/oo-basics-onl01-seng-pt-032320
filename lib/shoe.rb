62829cd48a2c5e921390017cf682e7a72b268cd2
bruce willis <fashun_vyctum@yahoo.com>
2020-04-07 03:14:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,18 +6,10 @@ class Shoe
     @brand = brand
   end
   
-  def condition(condition = "new")
-    @condition = condition
-  end
-  
-  def condition
-    @condition
-  end
-  
   def cobble
     puts "Your shoe is as good as new!"
   end
   
 end
 
-
+shoe.new()
