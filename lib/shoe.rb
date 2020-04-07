54cb8b995969aa2119ade7e7f3f1f018b275e298
bruce willis <fashun_vyctum@yahoo.com>
2020-04-07 03:05:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,27 +1,11 @@
 class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
   def initialize(brand = "Nike")
     @brand = brand
   end
   
-  def brand
-    @brand
-  end
   
-  def color=(color)
-    @color = color
-  end
-  
-  def color
-    @color
-  end
-  
-  def size=(size)
-    @size = size
-  end
-  
-  def size
-    @size
-  end
   
 end
